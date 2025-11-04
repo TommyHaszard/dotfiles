494dc7e6f91f57723899163e4e7884980fbf897d
ThomasHaszard <tommy45344@gmail.com>
2025-11-05 09:06:15 +1100

    add telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,11 +9,14 @@ vim.o.smartindent = true
 vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.number = true
+vim.o.winborder = "rounded"
 
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>')
+vim.keymap.set({'n', 'v', 'x'}, '<leader>p', '"+p<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
@@ -25,6 +28,9 @@ vim.pack.add({
 	{ src = "https://github.com/hrsh7th/nvim-cmp" },
 	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
 	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 })
 
 require "mason".setup()
@@ -35,6 +41,49 @@ require "mason-lspconfig".setup {
 		}
 	}
 }
+
+local map = vim.keymap.set
+
+local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"─", -- top
+			"│", -- right
+			"─", -- bottom
+			"│", -- left
+			"┌", -- top-left
+			"┐", -- top-right
+			"┘", -- bottom-right
+			"└", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			prompt_position = "top",
+		}
+	}
+})
+telescope.load_extension("ui-select")
+
+local builtin = require('telescope.builtin')
+map('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+map({ "n" }, "<leader>g", builtin.live_grep)
+map({ "n" }, "<leader>sb", builtin.buffers)
+map({ "n" }, "<leader>si", builtin.grep_string)
+map({ "n" }, "<leader>so", builtin.oldfiles)
+map({ "n" }, "<leader>sh", builtin.help_tags)
+map({ "n" }, "<leader>sm", builtin.man_pages)
+map({ "n" }, "<leader>sr", builtin.lsp_references)
+map({ "n" }, "<leader>sd", builtin.diagnostics)
+map({ "n" }, "<leader>si", builtin.lsp_implementations)
+map({ "n" }, "<leader>sT", builtin.lsp_type_definitions)
+map({ "n" }, "<leader>ss", builtin.current_buffer_fuzzy_find)
+map({ "n" }, "<leader>st", builtin.builtin)
+map({ "n" }, "<leader>sc", builtin.git_bcommits)
+map({ "n" }, "<leader>sk", builtin.keymaps)
+
 vim.lsp.enable({ "lua_ls" })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "java",
