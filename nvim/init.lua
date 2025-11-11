85946cf73d189234c424ad423ef3a925642c2113
thomas.haszard <thomas.haszard@ferag.com>
2025-11-11 20:51:14 +1100

    fix nvim-java
    fixed nvim-java to work with lombok by overriding the jdtls config and
    made changes to formatting for java files

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,13 +17,13 @@ vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/ribru17/bamboo.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = "main" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/hrsh7th/nvim-cmp" },
 	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
+	--	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
@@ -35,28 +35,46 @@ vim.pack.add({
 	{ src = "https://github.com/kndndrj/nvim-dbee" },
 	{ src = "https://github.com/nvim-java/lua-async-await" },
 	{ src = "https://github.com/mfussenegger/nvim-dap" },
- -- { src = "https://github.com/nvim-java/nvim-java-core" },
- --	{ src = "https://github.com/nvim-java/nvim-java-refactor" },
- -- { src = "https://github.com/nvim-java/nvim-java-test" },
- -- { src = "https://github.com/nvim-java/nvim-java-dap" },
- --	{ src = "https://github.com/nvim-java/nvim-java" },
+	{ src = "https://github.com/nvim-java/nvim-java-core" },
+	{ src = "https://github.com/nvim-java/nvim-java-refactor" },
+	{ src = "https://github.com/nvim-java/nvim-java-test" },
+	{ src = "https://github.com/nvim-java/nvim-java-dap" },
+	{ src = "https://github.com/nvim-java/nvim-java" },
 })
 
 local pm = require("plugin_manager")
 pm.add({ src = "https://github.com/Saghen/blink.cmp", tag = "v1.7.0" })
 
 require "mason".setup()
-require "mason-lspconfig".setup {
-	automatic_enable = {
-		exclude = {
-			"jdtls"
-		}
-	}
-}
+require("mason-lspconfig").setup({
+	handlers = {
+		['jdtls'] = function() end,
+	},
+})
 
--- require('java').setup()
--- vim.lsp.config("jdtls", {})
--- require('jdtls_setup').setup({})
+require('java').setup({
+	java_test = {
+		enable = true,
+		version = "0.43.1",
+	},
+	java_debug_adapter = {
+		enable = true,
+	},
+	spring_boot_tools = {
+		enable = true,
+	},
+	jdk = {
+		auto_install = false, -- We're using your custom JDK
+	},
+	notifications = {
+		dap = true,
+	},
+	lombok = {
+		enable = true,
+	},
+})
+
+require('jdtls_setup').setup({})
 
 require("nvim-autopairs").setup()
 require("oil").setup()
@@ -81,7 +99,7 @@ telescope.setup({
 			"┐", -- top-right
 			"┘", -- bottom-right
 			"└", -- bottom-left
-		},
+},
 		path_displays = { "smart" },
 		layout_config = {
 			prompt_position = "top",
@@ -91,12 +109,22 @@ telescope.setup({
 telescope.load_extension("ui-select")
 
 vim.lsp.enable({ "lua_ls" })
+
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "java",
-    callback = function()
-    	require('jdtls_setup').setup({})
-    end
+  pattern = "java",
+  callback = function()
+    vim.bo.autoindent = false
+    vim.bo.smartindent = false
+    vim.bo.cindent = false
+
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end,
 })
+
+
+
 
 vim.cmd("colorscheme bamboo")
 
--- nvim/lua/jdtls_setup.lua
+++ nvim/lua/jdtls_setup.lua
@@ -1,17 +1,18 @@
 local M = {}
 function M:setup()
 	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-	local workspace_dir = vim.fn.stdpath("data") ..
-			package.config:sub(1, 1) .. "jdtls-workspace" .. package.config:sub(1, 1) .. project_name
+	local sep = package.config:sub(1, 1)
+	local data_path = vim.fn.stdpath("data")
+	local workspace_dir = data_path .. sep .. "jdtls-workspace" .. sep .. project_name
 	local os_name = vim.loop.os_uname().sysname
 	local arch = vim.loop.os_uname().machine
+
 	-- Create workspace directory
 	vim.fn.mkdir(workspace_dir, "p")
-	local sep = package.config.sub(1, 1)
 
-	local jdtls_path = vim.fn.stdpath("data") .. sep .. "mason" .. sep .. "packages" .. sep .. "jdtls"
-	local launcher_jar = vim.fn.glob(jdtls_path ..
-		sep .. "plugins" .. sep .. "org.eclipse.equinox.launcher_*.jar")
+	local jdtls_path = data_path .. sep .. "mason" .. sep .. "packages" .. sep .. "jdtls"
+	local lombok_jar = data_path .. sep .. "mason" .. sep .. "packages" .. sep .. "jdtls" .. sep .. "lombok.jar"
+	local launcher_jar = vim.fn.glob(jdtls_path .. sep .. "plugins" .. sep .. "org.eclipse.equinox.launcher_*.jar")
 
 	if launcher_jar == "" then
 		vim.notify("jdtls launcher jar not found", vim.log.levels.ERROR)
@@ -19,8 +20,11 @@ function M:setup()
 	end
 
 	print("Launcher jar: " .. launcher_jar)
-  print("JDTLS Path: " .. jdtls_path)
-	print("Config path: " .. jdtls_path .. package.config:sub(1,1) .. "config_" .. (os_name == "Windows_NT" and "win" or os_name == "Linux" and "linux" or "mac"))
+	print("JDTLS Path: " .. jdtls_path)
+	print("Config path: " ..
+		jdtls_path ..
+		package.config:sub(1, 1) ..
+		"config_" .. (os_name == "Windows_NT" and "win" or os_name == "Linux" and "linux" or "mac"))
 
 	local config_dir
 	if os_name == "Linux" then
@@ -32,7 +36,14 @@ function M:setup()
 	end
 
 	vim.env.JAVA_HOME = "/opt/homebrew/opt/openjdk@21"
-	local root_dir = vim.fs.dirname(vim.fs.find({"gradlew", ".git", "mvnw", "pom.xml"}, { upward = true })[1])
+	local root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1])
+	local format_file = vim.fn.expand("~/.config/nvim/lua/Default.xml")
+	if vim.fn.filereadable(format_file) == 0 then
+		vim.notify("Format file not found at: " .. format_file, vim.log.levels.WARN)
+	else
+		print("Using format file: " .. format_file)
+	end
+
 	local config = {
 		cmd = {
 			"/opt/homebrew/opt/openjdk@21/bin/java",
@@ -43,36 +54,63 @@ function M:setup()
 			"-Dlog.level=ALL",
 			"-Xmx1g",
 			"--add-modules=ALL-SYSTEM",
-			"--add-opens",
-			"java.base/java.util=ALL-UNNAMED",
-			"--add-opens",
-			"java.base/java.lang=ALL-UNNAMED",
-			"-jar",
-			launcher_jar,
-			"-configuration",
-			jdtls_path .. sep .. config_dir,
-			"-data",
-			workspace_dir,
-			'-javaagent:' .. vim.fn.stdpath("data") .. sep .. "mason" .. sep .. "packages" .. sep .. "jdtls" .. sep .. "lombok.jar",
+			"--add-opens", "java.base/java.util=ALL-UNNAMED",
+			"--add-opens", "java.base/java.lang=ALL-UNNAMED",
+			-- CRITICAL: -javaagent MUST come BEFORE -jar
+			"-javaagent:" .. lombok_jar,
+			"-jar", launcher_jar,
+			"-configuration", jdtls_path .. sep .. config_dir,
+			"-data", workspace_dir,
 		},
 		root_dir = root_dir,
 		settings = {
 			java = {
+				jdt = {
+					ls = {
+						lombokSupport = {
+							enabled = true
+						}
+					}
+				},
 				format = {
 					enabled = true,
 					settings = {
-						url = vim.fn.expand("~/.config/nvim/lua/Default.xml"),
+						-- Use file:// protocol
+						url = "file://" .. format_file,
 						profile = "Default",
 					},
+					-- Add inline format settings
+					tabSize = 16,
+					insertSpaces = true,
+				},
+				-- Try inline settings as backup
+				configuration = {
+					updateBuildConfiguration = "automatic",
 				},
 			},
 		},
 		init_options = {
 			bundles = {},
 		},
+		-- Add on_attach to set format options
+		on_attach = function(client, bufnr)
+			-- Ensure formatting is enabled
+			if client.server_capabilities.documentFormattingProvider then
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end,
+				})
+			end
+		end,
 	}
+
 	print("Full command: " .. table.concat(config.cmd, " "))
-  require("jdtls").start_or_attach(config)
+	print("Lombok jar: " .. lombok_jar)
+	print("Format file: " .. format_file)
+
+	vim.lsp.config("jdtls", config)
 end
 
 return M
